258bf83354ffa72469dad5ebe069680869ed3acc
Dayne Dewar <daynedewar@yahoo.com>
2021-01-15 19:11:34 -0500

    commit tree 968a69bd2ecca265b08155928a6138ec0f8578c2

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,27 @@
 class Instructor
+    @@all = []
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end
 
+    def self.all
+        self.class.all
+    end
+
+    def passed_students
+        
+    end
+
+    def pass_student
+
+    end
+
+    def fail_student
+
+    end
+
+    def all_students
+
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,33 @@
 class Student
+    @all = []
+    def initialize(first_name)
+        @first_name = first_name
+        self.class.all << self
+    end
+
+    def self.all
+        self.class.all
+    end
+
+    def add_boating_test
+        test = self.new
+    end
+
+
+    end
+
+    def instructors
+
+    end
+
+    def self.find_student(name)
+        @@all.select {|student| student.first_name == name}
+    end
+
+    def grade_percentage
+
+    end
+
+
 
 end
